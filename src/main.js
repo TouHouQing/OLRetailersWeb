a6e38b10021e326fa9eb205200e112cf112e9cd1
touhouqing <qinghaoyang@foxmail.com>
2025-05-07 17:15:08 +0800

    使用pinia优化导航数据

--- src/main.js
+++ src/main.js
@@ -5,11 +5,6 @@ import App from './App.vue'
 import router from './router'
 import '@/styles/common.scss'
 
-import { getCategoryAPI } from './apis/testAPI'
-getCategoryAPI().then(res=>{
-  console.log(res);
-})
-
 const app = createApp(App)
 
 app.use(createPinia())
